443e65d4e6104a72f9f4dfcae1329bf25b956a05
subhamdash45 <mrsubhamdash1998@gmail.com>
2025-06-28 20:31:28 +0530

    pushed first recursion code

---
+++ Recursion/printFirstNNumbers.js
@@ -0,0 +1,18 @@
+// function printFirstNNumbers(limit, current = 1) {
+//   if (current > limit) {
+//     return;
+//   }
+//   console.log(current);
+//   printFirstNNumbers(limit, ++current);
+// }
+
+// printFirstNNumbers(10);
+
+function inReverse(number) {
+  if (number === 0) return;
+
+  console.log(number);
+  inReverse(--number);
+}
+
+inReverse(10);
