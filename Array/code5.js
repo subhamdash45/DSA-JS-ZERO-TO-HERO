b3c6311430a84a21badaee53df1131467d09751d
subhamdash45 <mrsubhamdash1998@gmail.com>
2025-07-22 21:05:33 +0530

    code related to array

---
+++ Array/code5.js
@@ -0,0 +1,17 @@
+// reverse string
+
+const arr = [1, 3, 5, 2, 3, 4, 6, 7, 3, 5, 6];
+// const arr = [1, 3, 5, 2, 3, 4, 6, 7, 3, 5];
+
+function recerseArray(nums) {
+  const mid = Math.floor(nums.length / 2);
+  for (let i = 0; i < mid; i++) {
+    console.log(i);
+    const temp = arr[i];
+    arr[i] = arr[arr.length - 1 - i];
+    arr[arr.length - 1 - i] = temp;
+  }
+}
+
+recerseArray(arr);
+console.log(arr);
