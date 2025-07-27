db70567e546fc9ca74cbbeab46849bd34a939c2b
subhamdash45 <mrsubhamdash1998@gmail.com>
2025-07-27 19:11:57 +0530

    added code related to arrays

---
+++ Array/code11.js
@@ -0,0 +1,40 @@
+// 136. Single Number
+
+// Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+// You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+// Example 1:
+// Input: nums = [2,2,1]
+// Output: 1
+// Example 2:
+// Input: nums = [4,1,2,1,2]
+// Output: 4
+
+// way--1
+
+// var singleNumber = function(nums) {
+//   const obj = {}
+//   for(let i=0;i<nums.length;i++){
+//    if(!obj[nums[i]]){
+//        obj[nums[i]] = 1
+//    }
+//    else{
+//       obj[nums[i]] += 1
+//    }
+//   }
+//   for(const key in obj){
+//        if(obj[key]===1){
+//            return Number(key)
+//        }
+//   }
+// };
+
+//way-2
+function singleNumber(nums) {
+  let xor = 0;
+  for (let i = 0; i < nums.length; i++) {
+    xor = xor ^ nums[i];
+  }
+  return xor;
+}
+console.log(singleNumber([1, 2, 3, 2, 3, 1, 5]));
---
+++ Array/code8.js
@@ -0,0 +1,50 @@
+// Move 0s
+// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+// Note that you must do this in-place without making a copy of the array.
+
+// Example 1:
+
+// Input: nums = [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+// Example 2:
+
+// Input: nums = [0]
+// Output: [0]
+
+const arr = [0, 1, 0, 3, 12];
+
+// way-1
+
+// function moveZeros(nums) {
+//   if (nums.length === 1) return nums;
+//   let j = 0;
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i]) {
+//       nums[j] = nums[i];
+//       if (i !== j) {
+//         nums[i] = 0;
+//       }
+//       j++;
+//     }
+//   }
+// }
+
+// way-2
+
+function moveZeros(nums) {
+  let j = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i]) {
+      nums[j] = nums[i];
+      j++;
+    }
+  }
+  while (j < nums.length) {
+    nums[j] = 0;
+    j++;
+  }
+}
+
+moveZeros(arr);
+console.log(arr);
